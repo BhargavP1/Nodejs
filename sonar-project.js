f2d9a2dc4d5882468b1971f78d415783a731bb0c
BhargavP1 <103743687+BhargavP1@users.noreply.github.com>
2022-07-21 10:48:19 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonar');
      sonarqubeScanner({
-       serverUrl: 'http://ec2-54.177.50.205.compute-1.amazonaws.com:9000',
+       serverUrl: 'http://ec2-54.241.187.91.compute-1.amazonaws.com:9000',
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : './**' // Entry point of your code
